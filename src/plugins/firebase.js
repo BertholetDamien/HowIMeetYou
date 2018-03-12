21578f7fc4832a008fbae0507ffe59b17d0c40b9
Bertholet Damien <bertholet.damien@gmail.com>
2018-03-12 18:25:57 -0400

    move settings for firebase

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,16 +1,8 @@
 import * as firebase from 'firebase';
 import 'firebase/firestore';
+import firebaseConfig from '../firebase-config.json';
 
-const config = {
-  apiKey: 'AIzaSyCvJ7kkRJUPhR5a4D3_SJ0s-3ARRlgvaSg',
-  authDomain: 'how-i-meet-you.firebaseapp.com',
-  databaseURL: 'https://how-i-meet-you.firebaseio.com',
-  projectId: 'how-i-meet-you',
-  storageBucket: '',
-  messagingSenderId: '809857462938',
-};
-
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 export default ({ Vue }) => {
   Vue.prototype.$firebase = firebase;
