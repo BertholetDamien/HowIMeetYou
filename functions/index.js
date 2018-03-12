5c96868ecdd477238e62ea95b1f7788618164473
Bertholet Damien <bertholet.damien@gmail.com>
2018-03-12 18:17:15 -0400

    Functions firebase

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,22 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {         
-  response.send("Hello from Firebase!");
-});
-
 exports.createStory = functions.firestore
     .document('stories/{storyID}')
     .onCreate(event => {
     const newValue = event.data.data();
 
     return event.data.ref.set({
-        first: newValue.first + "Test"
+        first: newValue.first + "Test",
+        author_id: 42,
+        status: 'active'
     }, {merge: true});
+});
+
+exports.onUserCreate = functions.auth.user().onCreate((event) => {
+    const user = event.data;
+
+    const email = user.email;
+    const displayName = user.displayName;
+
+
 });
