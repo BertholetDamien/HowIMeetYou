de4f82c390b8651090b6eb341b3bd20773198e69
Bertholet Damien <bertholet.damien@gmail.com>
2018-03-12 20:45:53 -0400

    Ignore file for merge

--- updateMaster.sh
+++ updateMaster.sh
@@ -1,4 +1,6 @@
 git merge --no-commit dev
 git reset HEAD src/firebase-config.json
 git checkout -- src/firebase-config.json
+git reset HEAD .firebaserc
+git checkout -- .firebaserc
 git commit -m "merged dev"
